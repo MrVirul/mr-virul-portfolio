aff587003f50e4e61d751f86667e1aa039794e8f
Virul Methdinu Meemana <virul.mmeemana@gmail.com>
2025-06-30 20:19:07 +0530

    refactor: reorder import statements for better readability in page.js

--- app/page.js
+++ app/page.js
@@ -1,11 +1,12 @@
- 'use client';
-import Header from "./components/Header";
- import Navbar from "./components/Navbar";
- export default function Home() {
+'use client';
+import Navbar from "./components/Navbar";
+import Header from "./components/Header"; // <-- Add this line
+
+export default function Home() {
   return (
     <>
-    <Navbar />
-    <Header />
+      <Navbar />
+      <Header />
     </>
   );
 }
