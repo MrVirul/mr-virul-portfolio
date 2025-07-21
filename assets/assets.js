5d773ddd101e355c1e30b22c2e5e6a467ac8a252
Virul Methdinu Meemana <virul.mmeemana@gmail.com>
2025-07-22 00:41:58 +0530

    feat: update asset references in workData; remove unused image placeholders

--- assets/assets.js
+++ assets/assets.js
@@ -1,29 +1,30 @@
-
 import logo_dark from "./logo_dark.png";
+import carcare_img from "./carcare_img.png";
+import blue_image from "./blue_image.png";
 
 export const assets = {
   user_image: "./user-image.png",
   logo_dark,
   profile_img: "./profile-img.png",
-  
+  carcare_img,
+  blue_image,
 };
 
 export const workData = [
   {
     title: "Car Care",
     description: " Car Service Maintenance system",
-    bgImage: "image.jpg",
+    bgImage: carcare_img, // Use asset reference here
     link: "https://github.com/mrbhanukab/CarCare",
-    techStack: ["Spring boots", "Java", "React", "Tailwind"], 
+    techStack: ["Spring boots", "Java", "React", "Tailwind"],
   },
-   {
+  {
     title: "MrVirul Portfolio",
     description: "My portfolio website",
-    bgImage: "image.jpg",
+    bgImage: blue_image, // Updated to use blue_image
     link: "https://github.com/MrVirul/mr-virul-portfolio",
-    techStack: [ "Java Script", "React", "Tailwind"],
+    techStack: ["Java Script", "React", "Tailwind"],
   },
-  
 ];
 
 export const serviceData = [
@@ -55,32 +56,32 @@ export const serviceData = [
   },
 ];
 
-export const infoList = [
-  {
-    icon: assets.code_icon,
-    iconDark: assets.code_icon_dark,
-    title: "Languages",
-    description: "HTML, CSS, JavaScript React Js, Next Js, Java, Python",
-  },
-  {
-    icon: assets.edu_icon,
-    iconDark: assets.edu_icon_dark,
-    title: "Education",
-    description:
-      "BSc (Hons) in Information Technology Undergraduate Specializing in Software Engineering at SLIIT",
-  },
-  {
-    icon: assets.project_icon,
-    iconDark: assets.project_icon_dark,
-    title: "Projects",
-    description: "Contribute to build car maintanance software",
-  },
-];
+// export const infoList = [
+//   {
+//     icon: assets.code_icon,
+//     iconDark: assets.code_icon_dark,
+//     title: "Languages",
+//     description: "HTML, CSS, JavaScript React Js, Next Js, Java, Python",
+//   },
+//   {
+//     icon: assets.edu_icon,
+//     iconDark: assets.edu_icon_dark,
+//     title: "Education",
+//     description:
+//       "BSc (Hons) in Information Technology Undergraduate Specializing in Software Engineering at SLIIT",
+//   },
+//   {
+//     icon: assets.project_icon,
+//     iconDark: assets.project_icon_dark,
+//     title: "Projects",
+//     description: "Contribute to build car maintanance software",
+//   },
+// ];
 
-export const toolsData = [
-  assets.vscode,
-  assets.firebase,
-  assets.mongodb,
-  assets.figma,
-  assets.git,
-];
+// export const toolsData = [
+//   assets.vscode,
+//   assets.firebase,
+//   assets.mongodb,
+//   assets.figma,
+//   assets.git,
+// ];
